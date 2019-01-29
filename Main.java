3f675ff10274bbf3593496b71ea359edb5a6ffcc
LaurWork <46980388+LaurWork@users.noreply.github.com>
2019-01-29 10:18:19 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+package com.jetbrains;
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class Main {
+
+    private static Object String;
+
+    public static void main(String[] args) {
+            Scanner keyboard = new Scanner(System.in);
+
+     out.print("Please list three items on your grocery shopping list.");
+
+        String= keyboard.nextLine();
+             String ItemOne = keyboard.next();
+             String ItemTwo = keyboard.next();
+             String ItemThree = keyboard.next();
+
+        out.println("Now, please enter the quantity of each item.");
+        out.println("How many " + ItemOne + "?");
+            int ItemOneAmt = keyboard.nextInt();
+        out.println("How many " + ItemTwo + "?");
+            int ItemTwoAmt = keyboard.nextInt();
+        out.println("How many " + ItemThree + "?");
+            int ItemThreeAmt = keyboard.nextInt();
+
+         out.println("Now, please enter the price of each item.");
+         out.println("How much does one " + ItemOne + " cost?");
+            float ItemOneCost = keyboard.nextFloat();
+         out.println("How much does one " + ItemTwo + " cost?");
+            float ItemTwoCost = keyboard.nextFloat();
+         out.println("How much does one " + ItemThree + " cost?");
+            float ItemThreeCost = keyboard.nextFloat();
+
+            float total = (ItemOneAmt * ItemOneCost) + (ItemTwoAmt * ItemTwoCost) + (ItemThreeAmt * ItemThreeCost);
+            out.println("Your total cost is $" + total + ".");
+    }
+}
