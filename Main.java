1b07f8dad8f77c972c5e146ce2dae0e9be55c78c
LaurWork <46980388+LaurWork@users.noreply.github.com>
2019-03-12 21:23:11 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.jetbrains;
+import java.util.Scanner;
+import static java.lang.System.out;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
