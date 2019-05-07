de36b3e2da958df2443b4ca414ebcb22e58fa80f
LaurWork <46980388+LaurWork@users.noreply.github.com>
2019-05-07 17:13:00 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,28 +1,49 @@
 package com.jetbrains;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import static java.lang.System.out;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Multiplication Tables");
-
-        System.out.println("How large would you like to see them? 1-20");
-        Scanner keyboard = new Scanner(System.in);
-        int size = keyboard.nextInt();
-
-        if(size>20)
-        System.out.println("Pick a number between 1 and 20.");
+        PostMenu postMenu = new PostMenu();
+        postMenu.runMenu();
 
 
-            for (int rows = 0; rows <= size; rows++) {
-                for (int columns = 0; columns <= size; columns++) {
-                    System.out.println(rows + "*" + columns + "=" + (rows * columns));
 
-                }
-                System.out.println();
+//        //Creates user objects
+//        User john = new User("url", "coolGuyJohn", "John", "Doe", "john@gmail.com");
+//        User mary = new User("url2", "maryMary", "Mary", "Bob", "mary@yahoo.com");
+//        User joe = new User("url", "JoeJo", "Joe", "Louis", "joe@hockey.com");
+//
+//        //Creates an array list of posts to store each post object
+//        List<Post> postList = new ArrayList<>();
+//
+//        //Creates post object without a url
+//        Post first = new Post(john, "My first post!");
+//
+//        //Adds the post to the list
+//        postList.add(first);
+//
+//        //Creates a post object with a url
+//        Post second = new Post(mary, "My second post", "www.awesome.com");
+//        postList.add(second);
+//        Post third = new Post (joe, "The third Post");
+//        postList.add(third);
+//        Post fourth = new Post(joe, "Another post", "www.google.com");
+//        postList.add(fourth);
+//        Post fifth = new Post(joe, "Joe posts a lot");
+//        postList.add(fifth);
 
-        }
+        //Loops through the list of posts, pulling out each post
+        //and storing it into a variable. Then prints the results
+        //of the toString method.
+//        for(Post post: postList){
+//            System.out.println(post.toString());
+//            System.out.println("");
+//        }
+
     }
 }
